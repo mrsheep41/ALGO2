c46c94491b835861c068c6cda8482813c17b9a13
mrsheep41 <37629255+mrsheep41@users.noreply.github.com>
2025-01-24 13:58:08 -0500

    Fixed spread problem
    Fixed problem of making unfulfillable orders when the underlaying ticker has extremely narrow spread

--- RIT API.py
+++ RIT API.py
@@ -2,11 +2,11 @@ import requests
 from time import sleep
 
 s = requests.Session()
-s.headers.update({'X-API-key': 'UE4DD4XK'}) # API Key from YOUR RIT Client
+s.headers.update({'X-API-key': 'G4C31V41'}) # API Key from YOUR RIT Client
 
 MAX_LONG_EXPOSUE = 25000
 MAX_SHOT_EXPOSUE = -25000
-ORDER_LIMIT = 250
+ORDER_LIMIT = 500
 
 u_case = 'http://localhost:9999/v1/case'
 u_book = 'http://localhost:9999/v1/securities/book'
@@ -87,7 +87,7 @@ def get_ticker_list(): #return a full list of the securities on the market
     return ticker_list
 ##################### DEFAULT FUNCTIONS #####################
 
-def is_buyer_market(ticker):
+def get_trade_vol(ticker):
     book = get_book(ticker)
     
     bid_vol = 0.0
@@ -99,24 +99,32 @@ def is_buyer_market(ticker):
     for item in book['asks']: #pull the total bid volume of the ticker
         ask_vol += item['quantity'] - item['quantity_filled']
 
-    return bid_vol < ask_vol
-    print('The bid vol for ' + ticker + ' is ' + str(bid_vol) + ' and the ask vol is ' + str(ask_vol))
+    return bid_vol, ask_vol
+    #print('The bid vol for ' + ticker + ' is ' + str(bid_vol) + ' and the ask vol is ' + str(ask_vol))
 
+def is_super_buyer(bvol, avol): #Check if its a strong buyer's market
+    return bvol / avol <= 0.70
+
+def is_super_seller(bvol, avol): #Check if its a strong seller's market
+    return avol / bvol <= 0.70
+
+def is_competitive(bp, ap): #Check if the bid ask spread is narrow
+    return 0.03 >= abs(bp - ap)
 
 def over_protection(limit):
     securities = s.get(u_securities).json()
     threshold = get_position() / limit
     
-    if threshold > 0.95:
+    if threshold > 0.95: #If the total position reaches 95% of the limit
         resp = s.post(u_cancel, params = {'all': 1})
         for item in securities:
             position = item['position']
-            if position > 0.0:
+            if position > 0.0: #Dump long position if the underlaying ticker has positive position
                 resp = s.post(u_orders, params = {'ticker': item['ticker'], 'type': 'MARKET', 'quantity': abs(position) * 0.2, 'action': 'SELL'})
-                print(str(item['ticker']) + 'Long Position Exploded, performing short')
-            elif position < 0.0:
+                print(str(item['ticker']) + ' long Position Exploded, short dumping')
+            elif position < 0.0: #Dump short position if the underlaying ticker has negative position
                 resp = s.post(u_orders, params = {'ticker': item['ticker'], 'type': 'MARKET', 'quantity': abs(position) * 0.2, 'action': 'BUY'})
-                print(str(item['ticker']) + 'Short Position Exploded, performing long')
+                print(str(item['ticker']) + ' short Position Exploded, long dumping')
 
 
 ##################### STRAT FUNCTION #####################
@@ -129,13 +137,26 @@ def main():
             ticker_symbol = ticker_list[i]
             position = get_position()
             best_bid_price, best_ask_price = get_bid_ask(ticker_symbol)
+            bid_vol, ask_vol = get_trade_vol(ticker_symbol)
             
-            if is_buyer_market(ticker_symbol):
-                resp = s.post('http://localhost:9999/v1/orders', params = {'ticker': ticker_symbol, 'type': 'LIMIT', 'quantity': ORDER_LIMIT, 'price': best_bid_price, 'action': 'BUY'})
-                print('Longing ' + str(ticker_symbol))
+            
+            if is_super_buyer(bid_vol, ask_vol):
+                #Take advantage when it is a buyer's market
+                resp = s.post(u_orders, params = {'ticker': ticker_symbol, 'type': 'LIMIT', 'quantity': ORDER_LIMIT * 1.5, 'price': best_bid_price - 0.6, 'action': 'BUY'})
+                print('A SUPER BUYERS MARKET FOR ' + str (ticker_symbol) + '! Longing ' + str(ORDER_LIMIT * 1.3) + ' at ' + str(best_bid_price - 0.6))
+            elif is_super_seller(bid_vol, ask_vol):
+                #Take advantage when it is a seller's market
+                resp = s.post(u_orders, params = {'ticker': ticker_symbol, 'type': 'LIMIT', 'quantity': ORDER_LIMIT * 1.5, 'price': best_ask_price + 0.6, 'action': 'SELL'})
+                print('A SUPER BUYERS MARKET FOR ' + str (ticker_symbol) + '! Shorting ' + str(ORDER_LIMIT * 1.3) + ' at ' + str(best_ask_price + 0.6))
+            elif is_competitive(best_bid_price, best_ask_price):
+                #To fix the problem of 0 trade in RY when spread is extremely narrow
+                resp = s.post(u_orders, params = {'ticker': ticker_symbol, 'type': 'LIMIT', 'quantity': ORDER_LIMIT * 0.9, 'price': best_bid_price - 0.3, 'action': 'BUY'})
+                resp = s.post(u_orders, params = {'ticker': ticker_symbol, 'type': 'LIMIT', 'quantity': ORDER_LIMIT * 0.9, 'price': best_ask_price + 0.3, 'action': 'SELL'})
+                print('Low spread for ' + str(ticker_symbol) + ' long & shorting at a reduced price')
             else:
-                resp = s.post('http://localhost:9999/v1/orders', params = {'ticker': ticker_symbol, 'type': 'LIMIT', 'quantity': ORDER_LIMIT, 'price': best_ask_price, 'action': 'SELL'})
-                print('Shorting ' + str(ticker_symbol))
+                resp = s.post(u_orders, params = {'ticker': ticker_symbol, 'type': 'LIMIT', 'quantity': ORDER_LIMIT, 'price': best_bid_price - 0.5, 'action': 'BUY'})
+                resp = s.post(u_orders, params = {'ticker': ticker_symbol, 'type': 'LIMIT', 'quantity': ORDER_LIMIT, 'price': best_ask_price + 0.5, 'action': 'SELL'})
+                print('Its nothing time, longing and shorting ' + str(ticker_symbol) + ' at best b&a price')
             
             sleep (0.5)
             over_protection(MAX_LONG_EXPOSUE)
